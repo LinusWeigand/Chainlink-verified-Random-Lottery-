e33e3a18569b5e7d6ace958565a1d0c832ea6cf7
Linus Weigand <linusweigand@protonmail.com>
2022-03-10 12:52:54 +0100

    Contract setup

---
+++ README.md
@@ -0,0 +1,3 @@
+1. Users can enter lottery with ETH based on USD fee
+2. An admin will choose when the lottery is over
+3. The lottery will select a random winner
---
+++ contracts/Lottery.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.6.6;
+
+contract Lottery {
+    address payable[] public players;
+    uint256 usdEntryFee;
+
+    constructor() public {
+        usdEntryFee = 50 * (10**18);
+    }
+
+    function enter() public payable {
+        // $50 minimum
+
+        players.push(msg.sender);
+    }
+
+    function getEntranceFee() public view returns (uint256) {}
+
+    function startLottery() public {}
+
+    function endLottery() public {}
+}
