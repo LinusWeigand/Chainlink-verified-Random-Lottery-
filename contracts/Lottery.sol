46ce720af3f7bd990d085657d8a60e4e5d192eaa
Linus Weigand <linusweigand@protonmail.com>
2022-03-10 13:10:04 +0100

    Contract extension

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,11 +1,17 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.6.6;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract Lottery {
     address payable[] public players;
     uint256 usdEntryFee;
+    AggregatorV3Interface internal ethUsdPriceFeed;
 
-    constructor() public {
+    constructor(address _priceFeedAddress) public {
         usdEntryFee = 50 * (10**18);
+        ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
     }
 
     function enter() public payable {
